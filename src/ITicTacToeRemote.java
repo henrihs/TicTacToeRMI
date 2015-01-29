cb46740886a90cbdb5a1b26e1f276428be5fe191
henrihs <hsvendsen@gmail.com>
2015-01-29 18:10:56 +0100

    final

--- src/ITicTacToeRemote.java
+++ src/ITicTacToeRemote.java
@@ -5,6 +5,6 @@ public interface ITicTacToeRemote extends Remote {
 	
 	public void remoteSetCell(int x, int y, char mark) throws RemoteException;
 	
-	public void remoteLookup(ITicTacToeRemote obj) throws RemoteException;
+	public void createReferenceToClient(ITicTacToeRemote obj) throws RemoteException;
 	
 }
--- src/TicTacToeRemote.java
+++ src/TicTacToeRemote.java
@@ -1,7 +1,6 @@
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -23,29 +22,39 @@ public class TicTacToeRemote extends UnicastRemoteObject implements ITicTacToeRe
 			tictactoe.gameEnded();
 		}
 	}
+	
+	@Override
+	public void createReferenceToClient(ITicTacToeRemote obj) throws RemoteException {
+		
+		ITicTacToeRemote opponent = obj;
+		tictactoe = new TicTacToe(1, opponent);
+		tictactoe.setStatusMessage("Connected to client, wait for your opponent to make the first move!");		
+	}
 
 	/**
 	 * 
 	 * @param args
-	 *  Server: server <own ip>
-	 *  Client: client <own ip> <server ip>
+	 * @param Server: server <own ip:port>
+	 * @param Client: client <server ip:port>
 	 */
 	public static void main(String[] args) {
 		if (args.length < 2){
 			System.out.println("Too few arguments");
 			return;
 		}
+		
+		// We had to comment out this line because it raised an AccessControlException on the server
 //		System.setSecurityManager(new RMISecurityManager());
 
 		try {
 
 			TicTacToeRemote local = new TicTacToeRemote();
 			if (args[0].equals("server"))
-				rebind(args[1], local);
+				Naming.rebind("rmi://" + args[1] + "/ITicTacToeRemote", local);
 
 			if (args[0].equals("client")){
-				ITicTacToeRemote opponent = local.lookup(args[1]);
-				opponent.remoteLookup(local);
+				ITicTacToeRemote opponent = (ITicTacToeRemote) Naming.lookup("rmi://" + args[1] + "/ITicTacToeRemote");
+				opponent.createReferenceToClient(local);
 				tictactoe = new TicTacToe(0, opponent);
 				tictactoe.setStatusMessage("Connected to " + args[1] + ", do your first move!");
 			}
@@ -62,29 +71,5 @@ public class TicTacToeRemote extends UnicastRemoteObject implements ITicTacToeRe
 		}
 	}
 
-	public static void rebind(String url, TicTacToeRemote obj){
-		try {
-			Naming.rebind("rmi://" + url + "/ITicTacToeRemote", obj);
-
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public ITicTacToeRemote lookup(String url) throws MalformedURLException, RemoteException, NotBoundException{
-		return (ITicTacToeRemote) Naming.lookup("rmi://" + url + "/ITicTacToeRemote");
-	}
-
-	@Override
-	public void remoteLookup(ITicTacToeRemote obj) throws RemoteException {
-
-		ITicTacToeRemote opponent = obj;
-		tictactoe = new TicTacToe(1, opponent);
-		tictactoe.setStatusMessage("Connected to client, wait for your opponent to make the first move!");		
-	}
 
 }
