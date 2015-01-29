898ee4f6c82f12e027b401f086cda05dd31d4691
henrihs <hsvendsen@gmail.com>
2015-01-29 11:19:48 +0100

    added java project files

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		for (String s : args){
+			System.out.println(s);
+		}
+	}
+
+}
